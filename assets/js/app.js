011b69b014e910bdbf91d6188d74b01c85ec7d54
J0ssy <j.g.basterrica@gmail.com>
2017-11-06 11:54:55 -0300

    1ra parte lista

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,21 +2,30 @@ var buttonTweet = document.getElementById('btn');
 	buttonTweet.addEventListener('click', function() {
 	
 	//tenemos que tomar el texto ingresado en el textarea
-	var tweets = document.getElementById('tweets').value;
+	var tweetsTextArea = document.getElementById('tweets');
+	var tweets = tweetsTextArea.value;
+	
 	//limpiar el textarea
 	document.getElementById('tweets').value = '';
 
 	//contenedor que está en el html
-	var container = document.getElementById('container');
+	var container = document.getElementById('commentDiv');
 
 	//creamos el div que contiene cada comentario
 	var newTweet = document.createElement('div');
+	newTweet.className = "tweetText commentDivTweet";
+    
+    //Evento que cambia el color del botón al ingresar caracteres
+	tweetsTextArea.addEventListener('input', function() {
+		buttonTweet.classList.add('addTweet');
+	});
 
 	//validar que textarea tenga un mensaje
 	if (tweets.length == 0 || tweets == null) {
 		alert('Debes ingresar un mensaje');
 		return false;
 	} 
+
 	//nodo de texto del textarea
 	var textNewTweet = document.createTextNode(tweets);
 
@@ -27,12 +36,6 @@ var buttonTweet = document.getElementById('btn');
 
 		container.appendChild(newTweet);	
 
-	//Llamando al textarea
-	var textArea = document.getElementsByTagName('textarea');
-
-	//Evento que cambia el color del botón al ingresar caracteres
-	textArea.addEventListener('input', function() {
-		buttonTweet.classList.add('textTweet');
-	})
+	
 	
 })
