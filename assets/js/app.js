0549e7c4336a8f67b18aaeccb703971cff773329
J0ssy <j.g.basterrica@gmail.com>
2017-11-07 00:51:42 -0300

    Exercise complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,87 +1,90 @@
+//Variables globales
 var buttonTweet = document.getElementById('btn');
-	buttonTweet.addEventListener('click', function() {
-		var d = new Date();
-	    var n = d.toLocaleTimeString();
-	    var time = document.getElementById("time").innerHTML = n;
+var textarea = document.getElementById('tweets');
+var initialCounter = document.getElementById("counter");
+var heightLimit = 200;
 
-	/*//Fecha
-	buttonTweet.addEventListener('click', function() {
-	    var d = new Date();
-	    var n = d.toLocaleTimeString();
-	    document.getEle..., ,,mentById("time").innerHTML = n;
-	});*/	                              
+//Evento de click para generar nuevo tweet
+buttonTweet.addEventListener('click', function() {
+	//Hora del tweet
+	var d = new Date();
+	var n = d.toLocaleTimeString();
 
-	//tenemos que tomar el texto ingresado en el textarea
-	var tweetsTextArea = document.getElementById('tweets');
-	var tweets = tweetsTextArea.value;
+	//Tenemos que tomar el texto ingresado en el textarea
+	var tweets = textarea.value;
 	
-	//limpiar el textarea
-	document.getElementById('tweets').value = '';
+	//Limpiar el textarea
+	textarea.value = '';
 
-	//contenedor que está en el html
+	//Deshabilitar el botón y el textarea
+	buttonTweet.disabled = true;
+	buttonTweet.classList.add("disableBtn");
+	textarea.classList.add('disabled');
+
+	//Contenedor que está en el html
 	var container = document.getElementById('commentDiv');
 
-	//creamos el div que contiene cada comentario
+	//Creamos el div que contiene cada tweet
 	var newTweet = document.createElement('div');
 	newTweet.className = "tweetText commentDivTweet";
-    
-    //Evento que cambia el color del botón al ingresar caracteres
-	tweetsTextArea.addEventListener('input', function() {
-		buttonTweet.classList.add('addTweet');
-	});
 
-
-	//nodo de texto del textarea
-	var textNewTweet = document.createTextNode(tweets);
+	//Nodo de texto del textarea
+	var textNewTweet = document.createTextNode(n + ' | ' + tweets); //Agregando hora + tweet
 
 	//Pasamos elementos
 	var contenedorElemento = document.createElement('p');
-		contenedorElemento.appendChild(textNewTweet);
-		newTweet.appendChild(contenedorElemento);
+	contenedorElemento.appendChild(textNewTweet);
+	newTweet.appendChild(contenedorElemento);
 
-		container.appendChild(newTweet);	
-		document.getElementById("counter").innerHTML = 140 
+	container.appendChild(newTweet);
 	
-	var counter = initialCounter.innerHTML = 140;
+	//Reseteando contador
+	initialCounter.innerHTML = 140;
 
-})
+});
 
-/*Botón en proceso
-var tweets = document.getElementById('tweets').value;
+//Desactivar botón y border del textarea
+function disableTweet() {
+	var tweets = document.getElementById('tweets').value;
 
-if (tweets.length == 0 || tweets == null) {
-	document.getElementById("btn").disabled = true;
-    btn.classList.add("disableBtn");
+	if (tweets.length == 0 || tweets == null || tweets.length > 140) {
+		buttonTweet.disabled = true;
+		buttonTweet.classList.add("disableBtn");
+		textarea.classList.add('disabled');
 	} else {
-		document.getElementById("btn").disabled = false;
-		btn.classList.remove("disableBtn");
+		buttonTweet.disabled = false;
+		buttonTweet.classList.remove("disableBtn");
+		textarea.classList.remove('disabled');
 	}
-*/
+}
 
 //Ajuste de tamaño automático
-var textarea = document.getElementById('tweets');
-var heightLimit = 200; /* Maximum height: 200px */
-
-textarea.oninput = function() {
+function textareaResize() {
   textarea.style.height = ""; 
   textarea.style.height = Math.min(textarea.scrollHeight, heightLimit) + "px";
 };
 
-//Contador
-textarea.onkeydown = function(){
-var initialCounter = document.getElementById("counter");
-var counter = initialCounter.innerHTML = 140 - this.value.length;
-	
-	if (counter <= 20 && counter >= 11) {
-   		initialCounter.classList.remove('basecolor');
-   		initialCounter.classList.add('prewarning');
-   	} else if (counter <= 10 && counter > 0) {
-   		initialCounter.classList.remove('prewarning');
-   		initialCounter.classList.add('warning');
-   	} else if(counter > 20) {
-   		initialCounter.classList.remove('prewarning');
-   		initialCounter.classList.remove('warning');
-   		initialCounter.classList.add('basecolor');
-   	} 
-};
+//Contador de caracteres
+function tweetCounter() {
+	var textCount = textarea.value.length;
+	var counter = initialCounter.innerHTML = 140 - textCount;
 
+	// Cambiar colores del contador
+	if (textCount >= 120 && textCount <= 130) {
+		initialCounter.classList.remove('basecolor', 'warning');
+		initialCounter.classList.add('prewarning');
+	} else if (textCount >= 130) {
+		initialCounter.classList.remove('basecolor', 'prewarning');
+		initialCounter.classList.add('warning');
+	} else {
+		initialCounter.classList.remove('prewarning', 'warning');
+		initialCounter.classList.add('basecolor');
+	}
+}
+
+//Llamando a las funciones desactivar, contador y redimensionar textarea
+textarea.addEventListener('input', function() {
+	disableTweet();
+	tweetCounter();
+	textareaResize();
+});
